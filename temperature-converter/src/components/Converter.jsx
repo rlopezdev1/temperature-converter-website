2431d419d36e9cf4de5d96f8b2afade177383f1a
rlopezdev1 <rlpezsto@gmail.com>
2022-10-27 21:50:55 -0700

    Adjusted div styling

--- temperature-converter/src/components/Converter.jsx
+++ temperature-converter/src/components/Converter.jsx
@@ -3,7 +3,21 @@ import React from "react";
 
 const Converter = () => {
     return (<div className="Converter">
-        <h1>Hello World</h1>
+      <div className="temperaturebox">
+        <label>Degrees</label>
+        <input type="text"></input>
+      </div>
+      <div className="typebox">
+        <label>Type</label>
+        <select id="type">
+            <option value="Fahrenheit" selected>Fahrenheit</option>
+            <option value="Celcuis">Celcuis</option>
+        </select>
+        
+      </div>
+      <div className="buttonbox">
+        <button className="convertbutton">Convert</button>
+      </div>
     </div>)
 }
 
