46bd09c49084118417b4303486c5bbd756886909
rlopezdev1 <rlpezsto@gmail.com>
2022-10-28 21:14:09 -0700

    Added main temp conversion logic

--- temperature-converter/src/components/Converter.jsx
+++ temperature-converter/src/components/Converter.jsx
@@ -1,29 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from 'react-dom/client';
 
 
 const Converter = () => {
+    const [temp , setTemp] = useState("");
+    const [type, setType] = useState("");
+
+
+
+    function handleChange(event){
+      const { name, value } = event.target;
+    
+      setTemp(value)
+
+    }
+
+    function handleType(event){
+      const { name, value } = event.target;
+      setType(value);
+    }
+
+    function convertTemp(){
+      var results = "";
+      const root = ReactDOM.createRoot(
+        document.getElementById('results')
+      );
+      
+
+      if(type==""){
+        const errornum = <h4 style={{visibility: 'visible'}}>Enter a valid number</h4>;
+        root.render(errornum);
+
+      } else if(type=="Fahrenheit"){
+        var numCel = parseFloat(temp);
+        results = ((numCel - 32)/1.8).toFixed(2) + '\u00B0 Celsious';
+        const element = <h2 style={{visibility: 'visible'}}>{results}</h2>;
+        root.render(element)
+        
+      } else {
+        var numFah = parseFloat(temp);
+        results = ((numFah * 9)/5 + 32).toFixed(2) + `\u00B0 Fahrenheit`;
+        const element = <h2 style={{visibility: 'visible'}}>{results}</h2>;
+        root.render(element)
+        
+      } 
+       
+     
+      
+
+      
+    }
+
     return (<div className="Converter">
       <div className="temperaturebox">
         <label>Degrees</label>
-        <input type="text"></input>
+        <input type="text" name="temperature" maxLength={5} onChange={handleChange} value={temp}></input>
       </div>
       <div className="typebox">
         <label>Type</label>
-        <select id="type">
-            <option value="Fahrenheit" selected>Fahrenheit</option>
+        <select id="type" onChange={handleType} defaultValue={"DEFAULT"} required>
+            <option disabled value="DEFAULT">Select Type</option>
+            <option value="Fahrenheit">Fahrenheit</option>
             <option value="Celcuis">Celcuis</option>
         </select>
         
       </div>
       <div className="buttonbox">
-        <button className="convertbutton">Convert</button>
+        <button className="convertbutton" onClick={() => convertTemp()}>Convert</button>
       </div>
-      <div className="results">
-            <h1>Results</h1>
+      <div className="resultsBox">
+            <h1 id="results">Results</h1>
       </div>
       
     </div>)
 }
+
+
+
+
+
+
+
+
+
 
 export default Converter
 
